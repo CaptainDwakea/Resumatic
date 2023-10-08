0040b37d378464db8990ae31d5cd029b9a3db2ce
shreyachak1712 <shreyachakraborty1712@gmail.com>
2023-10-08 18:00:07 +0530

    styling of login page

--- php pages/index.php
+++ php pages/index.php
@@ -1,27 +1,78 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>LOGIN</title>
-	<link rel="stylesheet" type="text/css" href="../css pages/login.css">
-</head>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> Login and Registration Form in HTML & CSS | CodingLab </title>
+    <link rel="stylesheet" href="../css pages/login.css">
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
 <body>
-     <form action="login.php" method="post">
-     	<h2>LOGIN</h2>
-     	<?php if (isset($_GET['error'])) { ?>
-     		<p class="error"><?php echo $_GET['error']; ?></p>
-     	<?php } ?>
-     	<label>User Name</label>
-     	<input type="text" name="uname" placeholder="User Name"><br>
 
-     	<label>Password</label>
-     	<input type="password" name="password" placeholder="Password"><br>
-
-     	<button type="submit">Login</button>
-		 <a href="signup.php" class="ca">Create an account</a>
-		 <div class="circle">
-    <img src="../images/logonew.jpeg" alt="">
-    <p> AN ONLINE RESUME BUILDER TO HELP YOU CREATE A PROFESSIONAL LOOKING RESUME QUICKLY AND EASILY</p>
+  <div class="container">
+    <input type="checkbox" id="flip">
+    <div class="cover">
+      <div class="front">
+        <img class= "img" src="../images/front.jpeg" alt="">
+        <div class="text">
+          <span class="text-1">An Online Resume Builder <br>To help you create a Professional looking Resume<br>Quickly and Easily</span>
+          <span class="text-2">Let's start building your resume</span>
+        </div>
+      </div>
+      <div class="back">
+        <img class="backimg" src="../images/back.jpeg" alt="">
+        
+      </div>
     </div>
-     </form>
+    <div class="forms">
+        <div class="form-content">
+          <div class="login-form">
+			<form action="login.php" method="post">
+            <div class="title">Login</div>
+          <form action="#">
+            <div class="input-boxes">
+              <div class="input-box">
+                <i class="fas fa-user"></i>
+                <input type="text" name="uname" placeholder="Enter your Username" required>
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" placeholder="Enter your password" required>
+              </div>
+              <div class="text"><a href="#">Forgot password?</a></div>
+              <div class="button input-box">
+                <input type="submit" value="Login">
+              </div>
+              <div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+            </div>
+        </form>
+      </div>
+        <div class="signup-form">
+          <div class="title">Signup</div>
+        <form action="#">
+            <div class="input-boxes">
+              <div class="input-box">
+                <i class="fas fa-user"></i>
+                <input type="text" name="name" placeholder="Enter your name" required>
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="text" name="password" placeholder="Enter your Password" required>
+              </div>
+              <div class="input-box">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="re_password"  placeholder="Re-Enter your password" required>
+              </div>
+              <div class="button input-box">
+                <input type="submit" value="Signup">
+              </div>
+              <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+            </div>
+      </form>
+    </div>
+    </div>
+    </div>
+  </div>
 </body>
 </html>
